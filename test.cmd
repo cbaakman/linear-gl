b866643af990e2dad2036f025fea523df89b7a45
cbaakman <cbaakman@gmail.com>
2018-09-05 23:44:34 +0200

    script to run the tests in windows

---
+++ test.cmd
@@ -0,0 +1,11 @@
+@echo off
+
+set CXX=g++
+set CFLAGS=-std=c++17
+
+if not exist bin (mkdir bin)
+
+for %%t in (mat) do (
+    echo test: %%t
+    %CXX% %CFLAGS% -I include tests/test_%%t.cpp -lboost_unit_test_framework -o bin/test_%%t && bin\test_%%t
+)
